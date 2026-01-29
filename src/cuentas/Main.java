1018c488a6caf0681931beca488885fe45f5a3bc
Miguelfc2005 <miguelfc2005@gmail.com>
2026-01-29 10:11:58 +0100

    Refactor: operativa_cuebta con parametro cantidad

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -3,23 +3,32 @@ package cuentas;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta cuenta1;
-        double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        CCuenta cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789", 2500,0);
+
+        float cantidad = 695;
+
+        operativa_cuenta(cuenta1, cantidad);
+    }
+
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+
+        System.out.println("El saldo actual es " + cuenta1.estado());
 
         try {
-            cuenta1.retirar(2300);
+            cuenta1.retirar(cantidad);
+            System.out.println("Retirada realizada correctamente");
         } catch (Exception e) {
-            System.out.print("Fallo al retirar");
+            System.out.println("Fallo al retirar");
         }
+
         try {
-            System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
+            System.out.println("Ingreso realizado correctamente");
         } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
+            System.out.println("Fallo al ingresar");
         }
+
+        System.out.println("Saldo final: " + cuenta1.estado());
     }
 }
